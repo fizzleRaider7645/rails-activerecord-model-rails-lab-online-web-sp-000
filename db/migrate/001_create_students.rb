3264e5503195ecc6379778cbd86d0488ced53995
Douglas Uretsky <douglasuretsky@gmail.com>
2019-01-08 16:20:47 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :first_name
-      t.text :description
+      t.string :last_name
  
       t.timestamps null: false
     end
